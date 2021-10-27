e1226a19ee26bfd563dcf9c39a3069e3369584b3
Hoseong Kim <86637300+ho323@users.noreply.github.com>
2021-10-27 10:49:48 +0900

    완주하지 못한 선수

---
+++ programmers/42576.py
@@ -0,0 +1,9 @@
+# 프로그래머스 완주하지 못한 선수
+# https://programmers.co.kr/learn/courses/30/lessons/42576
+def solution(participant, completion):
+    participant.sort()
+    completion.sort()
+    for i, j in zip(participant, completion):
+        if i != j:
+            return i
+    return participant[-1]
