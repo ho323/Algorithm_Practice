98369137948b625957569997b63221c371adbffe
Hoseong Kim / 김호성 <86637300+ho323@users.noreply.github.com>
2021-12-20 21:41:31 +0900

    프로그래머스 파이썬 짝수와 홀수

---
+++ programmers/p12937[2021.12.20].py
@@ -0,0 +1,8 @@
+# 프로그래머스 파이썬 짝수와 홀수
+# https://programmers.co.kr/learn/courses/30/lessons/12937
+def solution(num):
+    if num % 2 == 0:
+        answer = 'Even'
+    else:
+        answer = 'Odd'
+    return answer
